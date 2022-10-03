f424f76ba2cde62ae29acb440c230769e1848dd5
Maximilian Wernz <mawernz@outlook.de>
2022-10-03 22:45:56 +0200

    test commit

--- led.cpp
+++ led.cpp
@@ -4,7 +4,7 @@ void led(const char* color) {
     std::unordered_map<std::string, Color_t> colors = {{"red", Color(255, 0, 0)}, {"blue", Color(0, 0, 255)}, 
                                             {"green", Color(0, 255, 0)}, {"orange", Color(255, 35, 0)}, {"white", Color(255, 255, 255)}, {"warm", Color(255, 50, 10)}};
     NeoPixel* led = new NeoPixel(168);
-
+    puts(color);
     if (strcmp(color, "clear") == 0) {
         led->clear();
         led->show();
